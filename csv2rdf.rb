0baa6072a64319fe14bdf444bf7fc54773ee0d4c
benjab <benjab@benjab-kontor.(none)>
2011-09-16 10:59:40 +0200

    first commit

---
+++ csv2rdf.rb
@@ -0,0 +1,101 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'csv'
+require 'rdf'
+require 'rdf/rdfxml'
+require 'rdf/n3'
+require 'rdf/ntriples'
+
+def usage(s)
+    $stderr.puts(s)
+    $stderr.puts("Usage: \n")
+    $stderr.puts("#{File.basename($0)} -i input_file.csv -o output_file -b base_uri -t rdf_type [-r recordlimit]\n")
+    $stderr.puts("  -i input_file must be comma-separated file\n")
+    $stderr.puts("  -o output_file extension can be either .rdf (slooow) .n3 (sloow) or .nt (very fast)\n")
+    $stderr.puts("  -b base_uri must be uri\n")
+    $stderr.puts("  -t rdf_type must be uri\n")    
+    $stderr.puts("  -r [number] stops processing after given number of records\n")
+    exit(2)
+end
+
+loop { case ARGV[0]
+    when '-i':  ARGV.shift; $input_file  = ARGV.shift
+    when '-o':  ARGV.shift; $output_file = ARGV.shift
+    when '-b':  ARGV.shift; $base_uri    = ARGV.shift
+    when '-t':  ARGV.shift; $rdf_type    = ARGV.shift
+    when '-r':  ARGV.shift; $recordlimit = ARGV.shift.to_i # force integer
+    when /^-/:  usage("Unknown option: #{ARGV[0].inspect}")
+    else 
+      if !$input_file || !$output_file || !$base_uri || !$rdf_type then usage("Missing argument!\n") end
+    break
+end; }
+
+class String
+  def strip_leading_and_trailing_punct
+    str = self.sub(/[\.:,;\/\s\)\]]\s*$/,'').strip
+    return str.strip.sub(/^\s*[\.:,;\/\s\(\[]/,'')
+  end  
+end
+
+class RDFModeler
+  attr_reader :record, :statements, :uri
+  def initialize(record)
+    @record = record
+    construct_uri
+    @statements = []
+  end
+  
+  def construct_uri
+    @uri = RDF::URI.intern($base_uri)
+    id = "#{@record}"
+
+    #id = "#{@record[headers[0]]}"
+    id.gsub!(/[^\w\s\-ÆØÅæøå]/,"")
+    id.gsub!(/\s/,"_")
+    @uri += id
+  end
+  
+  def set_type(t)
+    @statements << RDF::Statement.new(@uri, RDF.type, t)
+  end
+  
+  def assert(p, o)
+    @statements << RDF::Statement.new(@uri, RDF::URI(p), o)
+  end
+  
+  def write_record
+      @statements.each do | statement |
+      #p statement
+        @@writer << statement
+      end
+  end
+end
+  
+count = 0
+
+csv = CSV.read($input_file)
+headers = csv.shift.map {|i| i.to_s }
+string_data = csv.map {|row| row.map {|cell| cell.to_s } }
+array_of_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
+# start writer handle
+RDF::Writer.open($output_file) do | writer |
+@@writer = writer
+
+  array_of_hashes.each do | record |
+    count += 1
+    if $recordlimit then break if count > $recordlimit end
+      # take the content of the first column to make record id
+      rdfrecord = RDFModeler.new(record["#{headers[0]}"])
+      rdfrecord.set_type(RDF::URI($rdf_type))
+      record.each do |k,v| 
+        unless v.empty?
+          v.strip_leading_and_trailing_punct
+          rdfrecord.assert(RDF::URI(k), RDF::Literal("#{v}"))
+        end
+      end 
+    
+    rdfrecord.write_record
+  end
+end # end writer loop
+puts "converted records: #{count}"
